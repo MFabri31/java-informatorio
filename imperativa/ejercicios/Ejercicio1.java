4a5437f004c9346f3da7ad9c89ffcaae86e55525
MFabri31 <medinajose3195@gmail.com>
2024-07-15 16:56:27 -0300

    01-programacion-imperativa - ejercicio numero 1

---
+++ imperativa/ejercicios/Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 6;
+
+        System.out.println(num1 + num2);
+    }
+}
---
+++ imperativa/ejercicios/JavaJedi.java
@@ -0,0 +1,8 @@
+public class JavaJedi {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 6;
+
+        System.out.println(num1 + num2);
+    }
+}
