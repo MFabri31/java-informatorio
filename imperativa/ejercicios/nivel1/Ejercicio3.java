5a6af12dfbd4644dfdc772fe2d18a029cec1cedd
MFabri31 <medinajose3195@gmail.com>
2024-07-16 18:56:48 -0300

    01-programacion-imperativa - ejercicio numero 3

---
+++ imperativa/ejercicios/nivel1/Ejercicio3.java
@@ -0,0 +1,23 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        char firstLetter = 'F';
+        char secondLetter = 'a';
+        char thirdLetter = 'b';
+        char fourthLetter = 'r';
+        char fifthLetter = 'i';
+        char sixthLetter = 'c';
+        char seventhLetter = 'i';
+        char eighthLetter = 'o';
+
+        System.out.println(firstLetter);
+        System.out.println(secondLetter);
+        System.out.println(thirdLetter);
+        System.out.println(fourthLetter);
+        System.out.println(fifthLetter);
+        System.out.println(sixthLetter);
+        System.out.println(seventhLetter);
+        System.out.println(eighthLetter);
+               
+    }
+}
