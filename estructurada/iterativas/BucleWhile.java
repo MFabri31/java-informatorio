c4958aef81ee5014a22c0a470a45ca1f2b06b179
MFabri31 <medinajose3195@gmail.com>
2024-07-19 17:04:01 -0300

    02-programacion-estructurada - estructuras iterativas

---
+++ estructurada/iterativas/BucleWhile.java
@@ -0,0 +1,13 @@
+package estructurada.iterativas;
+
+public class BucleWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 10) {
+            System.out.println("Hola soy el numero : " + contador);
+            contador = contador + 1;
+        }
+
+
+    }
+}
